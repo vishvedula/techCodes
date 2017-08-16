a15fdf424d0bee1c081533a960a0870872570ed1
Vishal Vedula <vishvedula@gmail.com>
2017-08-16 09:11:03 +0000

    FindNextLargestNumber_MultipleTestcases.java: Handle multiple testcases

---
+++ com/tech/practicePrograms/FindNextLargestNumber_MultipleTestcases.java
@@ -0,0 +1,61 @@
+/**
+ * Java code to find the next largest number.
+ * If theres no largest number, print -1
+ * 
+ * I/P: [4, 1, 2, 3]
+ * O/P: [-1, 2, 3, -1]
+ * 
+ * I/P: [1, 3, 2, 4]
+ * O/P: [3, 4, 4, -1]
+ * 
+ * I/P: [6, 8, 7, 10]
+ * O/P: [8, 10, 10, -1]
+ * 
+ * I/P: [6, 8, 1, 0]
+ * O/P: [8, -1, -1, -1]
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FindNextLargestNumber {
+
+	public static void main(String args[]) {
+		//System.out.println("Enter the number of testcases");
+		Scanner scan = new Scanner(System.in);
+		int noOfTestcases = scan.nextInt();
+		for (int l = 0; l < noOfTestcases; l++) {
+
+			// System.out.println("Enter the number of numbers");
+			int sizeOfArray = scan.nextInt();
+			int a[] = new int[sizeOfArray];
+			for (int k = 0; k < sizeOfArray; k++) {
+				a[k] = Integer.parseInt(scan.next());
+			}
+			int length = a.length;
+			List<Integer> list = new ArrayList<Integer>();
+			StringBuilder str = new StringBuilder();
+			for (int i = 0; i < length - 1; i++) {
+				boolean flag = true;
+				for (int j = i + 1; j < length; j++) {
+					if (a[j] > a[i]) {
+						list.add(a[j]);
+						str.append(a[j]);
+						str.append(" ");
+						flag = false;
+						break;
+					}
+				}
+				if (flag) {
+					list.add(-1);
+					str.append(-1);
+					str.append(" ");
+				}
+			}
+			list.add(-1);
+			str.append(-1);
+			System.out.println(str);
+		}
+	}
+}
