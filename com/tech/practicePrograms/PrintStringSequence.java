92e920769336e8c5c15005c51231a05c31bc95d0
Vishal Vedula <vishvedula@gmail.com>
2017-10-03 16:21:31 +0000

    PrintStringSequence: ("abcd") -->  "A-Bb-Ccc-Dddd" , example from Code Wars

---
+++ com/tech/practicePrograms/PrintStringSequence.java
@@ -0,0 +1,51 @@
+/**
+ * accum:
+Examples:
+
+Accumul.accum("abcd");    // "A-Bb-Ccc-Dddd"
+Accumul.accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+Accumul.accum("cwAt");    // "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+ * @author vivedula
+ *
+ */
+import java.util.Scanner;
+
+public class CodeWarsAccumul {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the string");
+		accum(scan.next());
+	}
+
+	 public static void accum(String s1) {
+		 String s = s1.toLowerCase();
+		 int length = s.length();
+		 char c[] = s.toCharArray();
+		 StringBuffer buf = new StringBuffer();
+		 
+		for (int i = 0; i < length; i++) {
+			String str = "";
+			int k = 0;
+			for (k = 0; k <= i; k++) {
+				if (k == 0) {
+					str += c[i];
+					str = str.toUpperCase();
+					buf.append(str);
+				} else {
+					str ="";
+					str+=c[i];
+					buf.append(str);
+				}
+			}
+			
+			buf.append('-');
+		}
+			String finalStr = (String) buf.toString();
+			System.out.println(finalStr.substring(0, finalStr.length() - 1));
+			
+	    }
+}
