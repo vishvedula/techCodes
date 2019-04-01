587acbd55508b480532c48a715f385c69c8c5cdb
vishvedula <45387278+vishvedula@users.noreply.github.com>
2019-04-01 15:08:51 +0530

    Update SumLargeNumbers.java

--- com/tech/practicePrograms/SumLargeNumbers.java
+++ com/tech/practicePrograms/SumLargeNumbers.java
@@ -1,5 +1,21 @@
+/**
+ * Given two numbers as strings.
+ *  The numbers may be very large (may not fit in long long int), the task is to find sum of these two numbers.
 
-		class SumLargeNumbers 
+Examples:
+
+Input  : str1 = "3333311111111111", 
+         str2 =   "44422222221111"
+Output : 3377733333332222
+
+Input  : str1 = "7777555511111111", 
+         str2 =    "3332222221111"
+Output : 7780887733332222
+
+ * @author vivedula
+ *
+ */
+class SumLargeNumbers 
 	{ 
 	// Function for finding sum of larger numbers  
 	static String findSum(String str1, String str2)  
