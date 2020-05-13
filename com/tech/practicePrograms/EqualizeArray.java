f1dec414731144c06cb7a6c7e325f076dfa491a6
vishvedula <45387278+vishvedula@users.noreply.github.com>
2020-05-13 17:15:10 +0530

    ==HackerRank Easy==

---
+++ com/tech/practicePrograms/EqualizeArray.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the equalizeArray function below.
+    static int equalizeArray(int[] arr) {
+        int len = arr.length;
+        int aux[] = new int[101];
+        for (int i = 0; i < len; i++) {
+            int index = arr[i];
+            aux[index]++;
+        }
+
+        int maxFreqCount = getMaxFreqCount(aux);
+        return len - maxFreqCount;
+    }
+
+    private static int getMaxFreqCount(int[] aux) {
+        int maxFreqCount = Integer.MIN_VALUE;
+        for (int freqCount : aux) {
+            if (freqCount > maxFreqCount) {
+                maxFreqCount = freqCount;
+            }
+        }
+        return maxFreqCount;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
