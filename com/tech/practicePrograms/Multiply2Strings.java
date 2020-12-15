e6c4d7a7eba3b2b03bf668ead00b297230c0dad0
vishvedula <45387278+vishvedula@users.noreply.github.com>
2020-12-15 17:17:40 +0530

    FaceBookInterview question
    We should multiply 2 strings without using any libraries.

---
+++ com/tech/practicePrograms/Multiply2Strings.java
@@ -0,0 +1,48 @@
+/*
+Given two numbers represented as strings, return multiplication of the numbers as a string.
+
+Analysis
+
+The key to solve this problem is multiplying each digit of the numbers at the corresponding positions and get the sum values at each position. That is how we do multiplication manually. 
+*/
+
+
+public class Multiply2Strings {
+   public static void main(String[] args) {
+  multiply("24","20");
+}
+   
+   public static String multiply(String num1, String num2) {
+    String n1 = new StringBuilder(num1).reverse().toString();
+    String n2 = new StringBuilder(num2).reverse().toString();
+ 
+    int[] d = new int[num1.length()+num2.length()];
+ 
+    //multiply each digit and sum at the corresponding positions
+    for(int i=0; i<n1.length(); i++){
+        for(int j=0; j<n2.length(); j++){
+            d[i+j] += (n1.charAt(i)-'0') * (n2.charAt(j)-'0');
+        }
+    }
+ 
+    StringBuilder sb = new StringBuilder();
+ 
+    //calculate each digit
+    for(int i=0; i<d.length; i++){
+        int mod = d[i]%10;
+        int carry = d[i]/10;
+        if(i+1<d.length){
+            d[i+1] += carry;
+        }
+        sb.insert(0, mod);
+    }
+ 
+    //remove front 0's
+    while(sb.charAt(0) == '0' && sb.length()> 1){
+        sb.deleteCharAt(0);
+    }
+ 
+    System.out.print(sb.toString());
+    return sb.toString();
+}
+   }
