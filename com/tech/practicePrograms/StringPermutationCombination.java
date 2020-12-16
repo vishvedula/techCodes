78f15bead92c472dc3b103592612e000983a36cd
vishvedula <45387278+vishvedula@users.noreply.github.com>
2020-12-16 23:46:12 +0530

    Update StringPermutationCombination.java
    This is an Amazon interview question of finding Permutations of given String

--- com/tech/practicePrograms/StringPermutationCombination.java
+++ com/tech/practicePrograms/StringPermutationCombination.java
@@ -27,11 +27,29 @@ abc acb bac bca cab cba
 adm amd dam dma mad mda
 abd adb bad bda dab dba
 
+Sample output for ABC:
+
+Recursive count-->1
+The set of words that are returned from previous recursion -->[]
+The final list of words that are computed -->[[C]]
+
+Recursive count-->2
+The set of words that are returned from previous recursion -->[C]
+The final list of words that are computed -->[[BC, CB]]
+
+Recursive count-->3
+The set of words that are returned from previous recursion -->[BC, CB]
+The final list of words that are computed -->[[ACB, BCA, ABC, CBA, BAC, CAB]]
+
+Permutations for ABC are: 
+[ACB, BCA, ABC, CBA, BAC, CAB]
+
 
  */
 
+import java.util.*;
 public class StringPermutations {
-	
+   private  static  int  count=0;
 		    public static Set<String> permutationFinder(String str) {
 		        Set<String> perm = new HashSet<String>();
 		        //Handling error scenarios
@@ -44,11 +62,16 @@ public class StringPermutations {
 		        char initial = str.charAt(0); // first character
 		        String rem = str.substring(1); // Full string without first character
 		        Set<String> words = permutationFinder(rem);
+              
+             		 ++count;
+		      System.out.println("Recursive count-->"+count);
+		      System.out.println("The set of words that are returned from previous recursion -->"+words);
 		        for (String strNew : words) {
 		            for (int i = 0;i<=strNew.length();i++){
 		                perm.add(charInsert(strNew, initial, i));
 		            }
 		        }
+              System.out.println("The final list of words that are computed -->"+Arrays.asList(perm));
 		        return perm;
 		    }
 		 
@@ -59,13 +82,16 @@ public class StringPermutations {
 		    }
 		 
 		    public static void main(String[] args) {
-		        String s = "AAC";
+		     String s1 = "ABC";
+		     System.out.println("\nPermutations for " + s1 + " are: \n" + permutationFinder(s1));
+
+		        /*String s = "AAC";
 		        String s1 = "ABC";
 		        String s2 = "ABCD";
 		        String s3 = "A";
 		        System.out.println("\nPermutations for " + s + " are: \n" + permutationFinder(s));
 		        System.out.println("\nPermutations for " + s1 + " are: \n" + permutationFinder(s1));
 		        System.out.println("\nPermutations for " + s2 + " are: \n" + permutationFinder(s2));
-		        System.out.println("\nPermutations for " + s2 + " are: \n" + permutationFinder(s3));
+		        System.out.println("\nPermutations for " + s2 + " are: \n" + permutationFinder(s3));*/
 		    }
 	}
