ac2ec9543e949d2a4fb6311e321c1b3d2d005360
vishvedula <45387278+vishvedula@users.noreply.github.com>
2020-12-21 11:53:14 +0530

    Print Christmas tree, hack on top of 180 rotation
    Print Christmas tree, hack on top of 180 rotation printPattern code

---
+++ com/tech/practicePrograms/PrintChristmasTree.java
@@ -0,0 +1,47 @@
+/*
+print following - 180 degree rotation
+        *
+      * * 
+    * * * 
+  * * * * 
+  
+  If we leverage the same code, and print "* ", * with space, then we would get a Christmas Tree :)
+*/
+
+public class PrintPattern2 {
+   public static void main(String[] args) {
+      int n = 5;
+      
+      printPattern(n);
+      
+      
+   }
+   
+   static void printPattern(int n){
+      
+      for(int i=0;i<n;i++)
+      {
+         int j = n-1; 
+         
+         // First fill all blanks in the array
+         int k=0;
+         
+         for(;k < j-i;k++) {
+            
+            System.out.print(" ");
+         }
+         
+         // For every same iteration fill the other segments with  *
+         
+         
+         for(int l=k;l<=n-1;l++){
+           // System.out.print("*"); // Code for 180* rotation
+           System.out.print("* "); // Give ("* ")with space to print a Christmas Tree :)
+         }
+         System.out.print("\n");
+       }
+      
+         System.out.print("\n");
+         
+    }
+}
