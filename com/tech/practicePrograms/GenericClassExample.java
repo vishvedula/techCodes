60470b6b184d71d34f3346f0ab08f31c6f026d27
vishvedula <45387278+vishvedula@users.noreply.github.com>
2021-01-13 16:13:39 +0530

    GenericClassExample, good example for learning Generic Classes
       Java Generic class example

---
+++ com/tech/practicePrograms/GenericClassExample.java
@@ -0,0 +1,34 @@
+/*
+   Java Generic class example
+*/
+
+public class GenericClassExample {
+  public static void main(String[] args) {
+
+    // initialize generic class
+    // with Integer data
+    GenericsClass<Integer> intObj = new GenericsClass<>(5);
+    System.out.println("Generic Class returns: " + intObj.getData());
+
+    // initialize generic class
+    // with String data
+    GenericsClass<String> stringObj = new GenericsClass<>("Java Programming");
+    System.out.println("Generic Class returns: " + stringObj.getData());
+  }
+}
+
+// create a generics class
+class GenericsClass<T> {
+
+  // variable of T type
+  private T data;
+
+  public GenericsClass(T data) {
+    this.data = data;
+  }
+
+  // method that return T type variable
+  public T getData() {
+    return this.data;
+  }
+}
