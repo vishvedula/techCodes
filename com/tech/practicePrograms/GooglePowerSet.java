abbf304feb381732cb47bef655a26fdec9f9071b
vishvedula <45387278+vishvedula@users.noreply.github.com>
2021-02-02 01:02:32 +0530

    GooglePowerSet --> Must and should to know Algorithm for solving many questions :) !
    Java program for power set 
     This problem was asked by Google.given {1,2,3} --> 
    {},{1},{2},{3},{1,2},{1,3},{2,3},{1,2,3} , power(2,setSize())
    
    BINARY Operations, GOOD TO KNOW

--- com/tech/practicePrograms/GooglePowerSet.java
+++ com/tech/practicePrograms/GooglePowerSet.java
@@ -1,42 +1,48 @@
-// Java program for power set 
-// This problem was asked by Google.given {1,2,3} --> {},{1},{2},{3},{1,2},{1,3},{2,3},{1,2,3} , power(2,setSize())
+
+/* 
+Java program for power set 
+ This problem was asked by Google.given {1,2,3} --> 
+{},{1},{2},{3},{1,2},{1,3},{2,3},{1,2,3} , power(2,setSize()) , 
+
+ie. power(2,3) -->  long powSetSize = (long)Math.pow(2, setSize);  --> 8
+
+BINARY Operations, GOOD TO KNOW
+*/
 
 import java .io.*; 
   
 public class GooglePowerSet { 
-      
-    static void printPowerSet(char []set, 
-                            int set_size) 
+   
+    // Driver program to test printPowerSet 
+    public static void main (String[] args) 
     { 
-          
-        /*set_size of power set of a set 
-        with set_size n is (2**n -1)*/
-        long pow_set_size =  
-            (long)Math.pow(2, set_size); 
-        int counter, j; 
+        int []set = {1,2,3}; 
+        printPowerSet(set, 3); 
+    } 
       
-        /*Run from counter 000..0 to 
-        111..1*/
-        for(counter = 0; counter <  
-                pow_set_size; counter++) 
+    static void printPowerSet(int []set, int setSize) 
+    { 
+        
+        long powSetSize = (long)Math.pow(2, setSize); 
+        int i, j; 
+      
+        for(i = 0; i < powSetSize; i++) 
+           
         { 
-            for(j = 0; j < set_size; j++) 
-            { 
-                /* Check if jth bit in the  
-                counter is set If set then  
-                print jth element from set */
-                if((counter & (1 << j)) > 0) 
-                    System.out.print(set[j]); 
+           System.out.print("{");
+            for(j = 0; j < setSize; j++) 
+            {  
+              
+                if((i & (1 << j)) > 0)  // BINARY Operations, GOOD TO KNOW
+                  
+                    System.out.print(set[j]+",");
+              
             } 
+            System.out.print("}");
               
             System.out.println(); 
         } 
     } 
       
-    // Driver program to test printPowerSet 
-    public static void main (String[] args) 
-    { 
-        char []set = {'1', '2', '3'}; 
-        printPowerSet(set, 3); 
-    } 
+   
 } 
